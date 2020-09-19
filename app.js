a7cf0c5b4347f76fb2f560c28760827e232d4e46
Palak921 <pariharpalak921@gmail.com>
2020-09-19 20:25:35 +0530

    deploying app1

--- app.js
+++ app.js
@@ -264,8 +264,12 @@ app.post("/login", function (req, res) {
     }
 });
 
-app.listen(3000, function () {
-    console.log("Server strarted on port succesfully");
+let port = process.env.PORT;
+if(port== null || port==""){
+    port=3000;
+}
+app.listen(port, function () {
+    console.log("Server has started succesfully!!!");
 });
 
 
